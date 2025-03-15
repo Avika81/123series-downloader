0406a4b0481b8f08065bfdca3b6ecb75f5d8fbbd
Avi Kadria <avi.kadria3@gmail.com>
2025-03-15 17:08:24 +0200

    Initial script, works=True (I think)

---
+++ download_videos.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import subprocess
+import json
+import sys
+
+from get_download_links import get_links_file_path
+
+FFMPEG_DOWNLOAD_LINE = 'ffmpeg -i "{url}" -c copy "{name}"'
+
+
+class DownloadVideos:
+    def __init__(self, serie_name):
+        self.serie_name = serie_name
+
+        with open(get_links_file_path(self.serie_name)) as f:
+            self.to_download = json.load(f)
+        self.output_dir = Path(__file__).parent / "series" / self.serie_name
+        if not self.output_dir.is_dir():
+            self.output_dir.mkdir()
+
+    def get_filename(self, name):
+        return self.output_dir / f"{name}.mp4"
+
+    def start_downloads(
+        self,
+    ):
+        processes = []
+
+        for name, url in self.to_download.items():
+            if not Path(self.get_filename(name)).exists():
+                processes.append(
+                    subprocess.Popen(
+                        FFMPEG_DOWNLOAD_LINE.format(
+                            url=url, name=self.get_filename(name)
+                        ),
+                        shell=True,
+                        stdout=subprocess.DEVNULL,
+                        stderr=subprocess.DEVNULL,
+                    )
+                )
+        return processes
+
+
+if __name__ == "__main__":
+    # usage: python download_videos.py <serie_name>
+    DownloadVideos(sys.argv[1]).start_downloads()
---
+++ get_download_links.py
@@ -0,0 +1,72 @@
+import time
+from seleniumwire import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import json
+from pathlib import Path
+from selenium.webdriver.common.by import By
+
+from series import THE_BLACKLIST, Serie
+
+URL_TEMPLATE = "https://123series.art/series/{name}/{season}-{episode}/"
+MAX_EPISODE = 400  # more than this and you should not watch this serie
+
+
+def get_links_file_path(serie_name):
+    path = Path(__file__).parent / "links" / f"{serie_name}.json"
+    if not path.is_file():
+        with open(path, "w") as f:
+            json.dump({}, f, indent=4)
+    return path
+
+
+def add_video(serie_name, name, url):
+    with open(get_links_file_path(serie_name)) as f:
+        to_download = json.load(f)
+
+    to_download[name] = url
+    with open(get_links_file_path(serie_name), "w") as f:
+        json.dump(to_download, f, indent=4)
+
+
+class EpisodeDoesNotExist(Exception):
+    pass
+
+
+class GetVideoLinks:
+    def __init__(self):
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    def get_download_links(self, url):
+        self.driver.get(url)
+        if self.driver.find_elements(By.ID, "main-wrapper"):
+            time.sleep(3)
+            for request in self.driver.requests:
+                if "index-v1-a1.m3u8" in request.url:
+                    # maybe there is more than 1?
+                    yield request.url
+        else:
+            raise EpisodeDoesNotExist(f"{url} has no presentation of video :/")
+
+
+def get_season_links(serie: Serie, season: int, gvl: GetVideoLinks):
+    for episode in range(1, MAX_EPISODE):
+        try:
+            for link in gvl.get_download_links(
+                URL_TEMPLATE.format(name=serie.name, season=season, episode=episode)
+            ):
+                add_video(
+                    serie_name=serie.human_name, name=f"{season}-{episode}", url=link
+                )
+                break
+                #TODO: support more than 1 link...
+        except EpisodeDoesNotExist:
+            break
+
+
+if __name__ == "__main__":
+    ## run stuffs :)
+    gvl = GetVideoLinks()
+    for season in range(1, 10):
+        get_season_links(THE_BLACKLIST, season=season)
+    gvl.driver.quit()
