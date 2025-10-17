5006e7b79ca31a758003c87ea17f0f2a8a779ab8
Avi Kadria <avi.kadria3@gmail.com>
2025-10-17 23:34:03 +0300

    should work - multiple at once

--- sync_subtitles.py
+++ sync_subtitles.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 import ffsubsync
 import os
@@ -5,12 +6,29 @@ import glob
 from persist_cache.persist_cache import cache
 
 
-def sync_all_series():
+def sync_all_series(executor=None):
     for base_path in glob.glob(str(Path(__file__).parent / "series" / "*" / "*")):
-        sync_all(base_path=base_path)
+        sync_all(base_path=base_path, executor=executor)
 
 
-def sync_all(base_path=str(Path(__file__).parent / "movies")):
+def sync_one(subtitles_path, video_path):
+    try:
+        sync_subtitles(
+            video_path=video_path,
+            subtitles_path=subtitles_path,
+        )
+    except ValueError as e:
+        if (
+            e.args[0]
+            == "Unable to detect speech. Perhaps try specifying a different stream / track, or a different vad."
+        ):
+            print(f"Error with video {video_path}. Removing it.")
+            os.remove(video_path)
+        else:
+            raise
+
+
+def sync_all(base_path=str(Path(__file__).parent / "movies"), executor=None):
     files = os.listdir(base_path)
     for video_file in files:
         if video_file.endswith(".mp4"):
@@ -18,20 +36,13 @@ def sync_all(base_path=str(Path(__file__).parent / "movies")):
             video_path = os.path.join(base_path, video_file)
             subtitles_path = os.path.join(base_path, subtitles_file)
             if subtitles_file in files:
-                try:
-                    sync_subtitles(
-                        video_path=video_path,
-                        subtitles_path=subtitles_path,
-                    )
-                except ValueError as e:
-                    if (
-                        e.args[0]
-                        == "Unable to detect speech. Perhaps try specifying a different stream / track, or a different vad."
-                    ):
-                        print(f"Error with video {video_path}. Removing it.")
-                        os.remove(video_path)
-                    else:
-                        raise
+                f = lambda: sync_one(
+                    subtitles_path=subtitles_path, video_path=video_path
+                )
+                if executor:
+                    executor.submit(f)
+                else:
+                    f()
 
 
 @cache
@@ -48,6 +59,8 @@ def sync_subtitles(subtitles_path, video_path):
 
 
 if __name__ == "__main__":
-    sync_all()
-    sync_all_series()
+    executor = ThreadPoolExecutor(max_workers=16)
+    sync_all(executor=executor)
+    sync_all_series(executor=executor)
+    executor.shutdown(wait=True)
     print("Done, your subtitle are snchronized and perfect (I hope) :)")
